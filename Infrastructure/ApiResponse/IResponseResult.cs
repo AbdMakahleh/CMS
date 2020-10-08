3d72fdbc6dbe1f26f21d22c965c6fd67b0686137
Makahleh <Makahleh@CS-IT-003>
2020-10-08 17:42:38 +0300

    fixes

--- Infrastructure/ApiResponse/IResponseResult.cs
+++ Infrastructure/ApiResponse/IResponseResult.cs
@@ -8,6 +8,7 @@ namespace Infrastructure.ApiResponse
     public interface IResponseResult 
     {
         public HttpStatusCode Status { get; set; }
+
         public string Message { get; set; }
         public string ErrorMessage { get; set; }
 
